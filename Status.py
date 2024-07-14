e7a447abcea69fdf73de99be125091ce799e5f64
leejinhyeok <tool7831@gmail.com>
2024-07-14 21:06:18 +0900

    create class

---
+++ Status.py
@@ -0,0 +1,23 @@
+
+class Status():
+    def __init__(self, status):
+        self.status = status
+        self.max_status = status
+        self.added_status = {'HP': 0, 'MP': 0, 'Strength': 0, 'Skill': 0, 'Dexterity': 0, 'Intelligence': 0, 'Luck': 0, 'Defense': 0, 'Speed': 0, 'Concentration': 0, 'Reaction': 0, 'MP Regeneration': 0, 'HP Regeneration': 0}
+        self.durable = list()
+
+    def change_max_value(self, name, value):
+        self.max_status[name] += value
+        self.status[name] += value
+
+    def get_status(self):
+        return self.status
+    
+    def change_added_value(self, name, value):
+        self.added_status[name] += value
+        self.status[name] += value
+
+    def change_hp(self, value):
+        self.status['HP'] += value
+        if self.status['HP'] > self.max_status['HP'] + self.added_status['HP']:
+            self.status['HP'] = self.max_status['HP'] + self.added_status['HP']
